34a2b5e9c3eb17b110766b585f781bb1480c0d61
Tama <tamawish@gmail.com>
2023-01-23 21:52:27 +0900

    controller,model,seeder,migrationサンプル作成

---
+++ src/app/Http/Controllers/NekochanController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Nekochan;
+use Illuminate\Http\Request;
+
+class NekochanController extends Controller
+{
+    public function index()
+    {
+        $nekochans = Nekochan::where('name', 'うめ')
+            ->orderBy('name')
+            ->take(10)
+            ->get()
+            ->toArray()
+            ;
+
+        dd($nekochans);
+
+        return view('welcome');
+    }
+}
